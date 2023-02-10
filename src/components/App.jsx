155f952d05ff7260db172e1cc981f9d2df31b33f
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-10 15:07:18 +0200

    optimize feedback options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,7 @@ export class App extends Component {
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.handleIncrement}
           />
         </Section>
